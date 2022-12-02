4f6d40a208d1d29cbbf7d7cf6ffe7d731654b131
elia-omeri <elia.omeri@crystal-system.eu>
2022-12-02 11:17:16 +0100

    working on repositories

---
+++ src/main/java/com/crystal/school/repository/SchoolRepository.java
@@ -0,0 +1,9 @@
+package com.crystal.school.repository;
+
+import com.crystal.school.model.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School,Integer> {
+}
---
+++ src/main/java/com/crystal/school/repository/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.crystal.school.repository;
+
+public interface StudentRepository {
+}
