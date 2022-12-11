b72f74bee66f8cc7d560e084450e6a337eafaf6a
Indrit Vaka <indrit.vaka-extern@renault.com>
2022-12-11 18:07:28 +0100

    LoginTest

--- src/test/java/com/crystal/school/service/PasswordServiceTest.java
+++ src/test/java/com/crystal/school/service/PasswordServiceTest.java
@@ -1,6 +1,7 @@
 package com.crystal.school.service;
 
 import com.crystal.school.excption.InvalidLength;
+import com.crystal.school.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,13 @@ class PasswordServiceTest {
     int length = 10;
     String password = "testPassword";
     String salt;
+
     @BeforeEach
     void beforeAll() throws InvalidLength {
         salt = passwordService.getSaltValue(10);
     }
+
+
     @Test
     void getSaltValue_ThrowsInvalidLength() {
         Assertions.assertThrows(InvalidLength.class, () -> passwordService.getSaltValue(-10));
@@ -32,6 +36,7 @@ class PasswordServiceTest {
         String securePassword = passwordService.generateSecurePassword(password, salt);
         Assertions.assertFalse(securePassword.contains(password));
     }
+
     @Test
     void generateSecurePassword_MoreCharacters() {
 
@@ -43,5 +48,24 @@ class PasswordServiceTest {
     void doesPasswordMatches() {
         String securePassword = passwordService.generateSecurePassword(password, salt);
         Assertions.assertTrue(passwordService.doesPasswordMatches(password, securePassword, salt));
+    }
+
+    @Test
+    void createUser() throws InvalidLength {
+        User user = new User();
+        user.setFirstName("Indrit");
+        user.setLastName("Vaka");
+        user.setEmail("indrit.vaka@crystal-system.eu");
+
+        String saltValue = passwordService.getSaltValue(30);
+        //The password we get from FE
+        String inputtedPassword = "DemoPassword";
+        // Only securedPassword will be saved to the DB
+        user.setSalt(saltValue);
+        String securePassword = passwordService.generateSecurePassword(inputtedPassword, saltValue);
+        user.setPassword(securePassword);
+
+        // Creating a login case
+        Assertions.assertTrue(passwordService.doesPasswordMatches(inputtedPassword,user.getPassword(),user.getSalt()));
     }
 }
